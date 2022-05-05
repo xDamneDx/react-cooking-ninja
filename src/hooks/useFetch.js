7c0a4729c982ef70974f941db186ec1f151630a7
xDamneDx <xdamnedx@gmail.com>
2022-05-05 10:35:45 +0300

    Added fetching data

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const fetchData = async () => {
+      setIsPending(true);
+
+      try {
+        const res = await fetch(url, { signal: controller.signal });
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        const json = await res.json();
+
+        setError(null);
+        setIsPending(false);
+        setData(json);
+      } catch (err) {
+        if (err.name === "AbortError") {
+          console.log("The fetch was aborted");
+        } else {
+          setIsPending(false);
+          setError("Could not fetch the data");
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      controller.abort();
+    };
+  }, [url]);
+
+  return { data, isPending, error };
+};
+
+export { useFetch };
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,21 @@
+import { useFetch } from "../../hooks/useFetch";
+
+// Styles:
 import "./Home.css";
 
-import React from "react";
-
 export default function Home() {
-  return <div>Home</div>;
+  const {
+    data: recipes,
+    isPending,
+    error,
+  } = useFetch("http://localhost:3000/recipes");
+
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipes &&
+        recipes.map((recipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
+    </div>
+  );
 }
