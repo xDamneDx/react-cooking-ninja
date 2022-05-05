d0531827d01c8cc5d098d3b3999223116d4ae01e
xDamneDx <xdamnedx@gmail.com>
2022-05-05 10:05:51 +0300

    Added React Router v.5.3.1  & setup pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+// Components:
+import Home from "./pages/home/Home";
+import Create from "./pages/create/Create";
+import Search from "./pages/search/Search";
+import Recipe from "./pages/recipe/Recipe";
+
+// Styles:
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
