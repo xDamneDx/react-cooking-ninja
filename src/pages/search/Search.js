8da0a6156dde334766489c5fc5788184ab6df662
xDamneDx <xdamnedx@gmail.com>
2022-05-06 10:00:28 +0300

    Added search results page

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,7 +1,28 @@
+import { useLocation } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+
+// Styles:
 import "./Search.css";
 
-import React from "react";
+// Components:
+import RecipeList from "../../components/RecipeList";
 
 export default function Search() {
-  return <div>Search</div>;
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get("q");
+  const {
+    data: recipes,
+    isPending,
+    error,
+  } = useFetch(`http://localhost:3000/recipes?q=${query}`);
+
+  return (
+    <div>
+      <h2 className="page-title">Recipes including "{query}"</h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipes && <RecipeList recipes={recipes} />}
+    </div>
+  );
 }
