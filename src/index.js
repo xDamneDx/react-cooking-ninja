62925cc5d4b56c457854be13cd9d0156c78672b0
xDamneDx <xdamnedx@gmail.com>
2022-05-06 10:49:45 +0300

    Added ThemeContext

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ThemeProvider } from "./context/ThemeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // React Router v5 doesn't work with React 18 StrictMode.
   // <React.StrictMode>
-  <App />
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
   // </React.StrictMode>
 );
