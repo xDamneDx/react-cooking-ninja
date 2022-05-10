15a91aad78357f3a4be66d52846211155a54dad6
xDamneDx <xdamnedx@gmail.com>
2022-05-10 10:08:04 +0300

    Added functionality for deleting data from firestore

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 import { useTheme } from "../hooks/useTheme";
+import Trashcan from "../assets/trashcan.svg";
+import { projectFirestore } from "../firebase/config";
 
 // Styles:
 import "./RecipeList.css";
 
 export default function RecipeList({ recipes }) {
   const { mode } = useTheme();
+  const handleClick = (id) => {
+    projectFirestore.collection("recipes").doc(id).delete();
+  };
 
   if (recipes.length === 0) {
     return <div className="error">No recipes to load...</div>;
@@ -19,6 +24,12 @@ export default function RecipeList({ recipes }) {
           <p>{recipe.cookingTime} to make.</p>
           <div>{recipe.method.substring(0, 100)}...</div>
           <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+          <img
+            className="delete"
+            src={Trashcan}
+            onClick={() => handleClick(recipe.id)}
+            alt="Delete icon"
+          />
         </div>
       ))}
     </div>
